d43f3caa72fa1dd6619d531c3f87dad8b27ab268
kilundi <muthiani42@gmail.com>
2024-03-31 11:52:29 +0300

    mailing

--- agents/views.py
+++ agents/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
+from django.conf import settings
 
 import random
 from django.views import generic
@@ -20,7 +21,7 @@ class AgentListView( OrganizorAndLoginRequiredMixin, generic.ListView):
         organization = self.request.user.userprofile
         return Agent.objects.filter( organization = organization)
 
-class AgentCreateView( OrganizorAndLoginRequiredMixin, generic.CreateView):
+class AgentCreateView(OrganizorAndLoginRequiredMixin, generic.CreateView):
     template_name = 'agents/agent_create.html'
     form_class = AgentModelForm
 
@@ -28,27 +29,31 @@ class AgentCreateView( OrganizorAndLoginRequiredMixin, generic.CreateView):
         return reverse("agents:agents_list")
 
     def form_valid(self, form):
-        user =form.save(commit=False)
+        user = form.save(commit=False)
         user.is_agent = True
         user.is_organizor = False
         user.set_password(f"{random.randint(0, 100000000)}")
         user.save()
 
         Agent.objects.create(
-            user = user,
-            organization = self.request.user.userprofile
-            )
-
-        send_mail(
-            subject = 'You are invited to be an agent',
-            message = 'You were added as an agent on COSSIM LIMITED. Please login to your account',
-            from_email = 'contact@cossim.com',
-            recipient_list = [user.email],
-
+            user=user,
+            organization=self.request.user.userprofile
         )
 
-        return super(AgentCreateView,self).form_valid(form)
+        send_mail(
+            subject='You are invited to be an agent',
+            message = (
+                f'You were added as an agent on COSSIM LIMITED. '
+                f'Please login <a href="https://crm-wtb4.onrender.com/login/">here</a> to your account. '
+                f'<p>Your username is <strong>{user.username}</strong> </p> '
+                f'Reset your password using this email <p>{user.email}</p>'
+            ),
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email],
+            fail_silently=False,  # Set to True to suppress exceptions if email sending fails
+        )
 
+        return super(AgentCreateView, self).form_valid(form)
 
 class AgentDetailView(OrganizorAndLoginRequiredMixin, generic.DetailView):
     template_name = 'agents/agent_details.html'
